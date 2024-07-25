367db7e51b43e95a5cfd70226a40b55b17892d24
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-07-25 23:47:24 +0200

    Initial VM support

---
+++ compiler/compiler.go
@@ -0,0 +1,93 @@
+package compiler
+
+import (
+	"fmt"
+
+	"github.com/javier-varez/monkey_interpreter/ast"
+	"github.com/javier-varez/monkey_interpreter/code"
+	"github.com/javier-varez/monkey_interpreter/object"
+	"github.com/javier-varez/monkey_interpreter/token"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		return c.Compile(node.Expr)
+
+	case *ast.InfixExpr:
+		err := c.Compile(node.LeftExpr)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.RightExpr)
+		if err != nil {
+			return err
+		}
+
+		switch node.OperatorToken.Type {
+		case token.PLUS:
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("Unhandled infix operator %s", node.OperatorToken.Type)
+		}
+
+		return nil
+
+	case *ast.IntegerLiteralExpr:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+	return nil
+}
+
+/// Operands is a list of operand offsets to the constants of the compiler
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	inst := code.Make(op, operands...)
+	pos := c.addInstruction(inst)
+	return pos
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	off := len(c.constants)
+	c.constants = append(c.constants, obj)
+	return off
+}
+
+func (c *Compiler) addInstruction(inst code.Instructions) int {
+	off := len(c.instructions)
+	c.instructions = append(c.instructions, inst...)
+	return off
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
---
+++ vm/vm.go
@@ -0,0 +1,84 @@
+package vm
+
+import (
+	"fmt"
+
+	"github.com/javier-varez/monkey_interpreter/code"
+	"github.com/javier-varez/monkey_interpreter/compiler"
+	"github.com/javier-varez/monkey_interpreter/object"
+)
+
+const STACK_SIZE = 2048
+
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+
+	stack []object.Object
+	sp    int // Always points to the next value. Top of stack is stack[sp-1]
+}
+
+func New(bytecode *compiler.Bytecode) *VM {
+	return &VM{
+		constants:    bytecode.Constants,
+		instructions: bytecode.Instructions,
+
+		stack: make([]object.Object, STACK_SIZE),
+	}
+}
+
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+		case code.OpAdd:
+			a, err := vm.pop()
+			if err != nil {
+				return err
+			}
+
+			b, err := vm.pop()
+			if err != nil {
+				return err
+			}
+
+			result := a.(*object.Integer).Value + b.(*object.Integer).Value
+			vm.push(&object.Integer{Value: result})
+		default:
+			return fmt.Errorf("Unhandled operation: %v", op)
+		}
+	}
+	return nil
+}
+
+func (vm *VM) push(ob object.Object) error {
+	if vm.sp >= len(vm.stack) {
+		return fmt.Errorf("Stack overflown")
+	}
+	vm.stack[vm.sp] = ob
+	vm.sp++
+	return nil
+}
+
+func (vm *VM) pop() (object.Object, error) {
+	if vm.sp == 0 {
+		return nil, fmt.Errorf("Stack underflown")
+	}
+	obj := vm.stack[vm.sp-1]
+	vm.sp--
+	return obj, nil
+}
+
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
